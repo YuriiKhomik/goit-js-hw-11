fce61beeca093f674ad99afdb5ce2eff7c82e1b2
Yurii Khomik <yu.hamster.pl@gmail.com>
2022-11-02 18:00:48 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { fetchImages, FetchImagesService } from './partials/fetchImagesService';
+import { ImagesApiService } from './partials/fetchImagesService';
 
-const fis = new FetchImagesService();
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+};
+
+const imagesApiService = new ImagesApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  imagesApiService.query = e.currentTarget.elements.searchQuery.value;
+
+  if (imagesApiService.query === '') {
+    return Notify.info('type smth');
+  }
+
+  imagesApiService.fetchImages();
+}
+
+function clearImagesContainer() {
+  refs.gallery.innerHTML = '';
+}
--- src/partials/fetchImagesService.js
+++ src/partials/fetchImagesService.js
@@ -1,4 +1,8 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api/';
+// axios.defaults.baseURL = BASE_URL;
+
 const API_KEY = '31038159-6af05e52325bfcde27c5dd046';
 const options = {
   headers: {
@@ -6,14 +10,46 @@ const options = {
   },
 };
 
-export class FetchImagesService {
+export class ImagesApiService {
   constructor() {
     this.searchQuery = '';
+    this.page = 1;
   }
 
   fetchImages() {
-    const url = `${BASE_URL}?q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
+    axios.defaults.baseURL = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
+    // const url = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
 
-    return fetch(url, options).then(response => response.json());
+    return axios.get().then(response => {
+      console.log(response);
+      return response.data;
+    });
+  }
+
+  // fetchImages() {
+  //   const url = `${BASE_URL}?q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+  //   return fetch(url, options)
+  //     .then(response => response.json())
+  //     .then(({ images }) => {
+  //       this.incrementPage();
+
+  //       return images;
+  //     });
+  // }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
   }
 }
